cd957be23db2a69780ff90ef687c088a0fe47152
AlfredYang1986 <yangyuanpig@gmail.com>
2017-03-31 20:17:56 +0800

    决胜网

--- src/main/scala/scraper_main.scala
+++ src/main/scala/scraper_main.scala
@@ -13,18 +13,19 @@ object scarper_main extends App {
 		print(
 			"""scraper options:
 		  |000. scraper for dianping (大众点评)
-		  |001. scraper for dianping (为艺)
-		  |002. scraper for other (以后添加其他)
-		  |003. scraper test
-		  |004. scraper for all
-		  |005. merge existing result
+		  |001. scraper for weiyi (为艺)
+		  |002. scraper for victory (决胜网)
+		  |003. scraper for other (以后添加其他)
+		  |004. scraper test
+		  |005. scraper for all
+		  |006. merge existing result
 		""".stripMargin)
 
 		val str = StdIn.readLine()
 		val op = str.toInt
 
 		val path = "src/main/resources/"
-		val lst = "dianping-sketch.json" :: "weiyi-sketch.json" :: "other.json" :: "test.json" :: Nil
+		val lst = "dianping-sketch.json" :: "weiyi-sketch.json" :: "webvictory.json" :: "other.json" :: "test.json" :: Nil
 
 		val sys = ActorSystem("scraper")
 		val s = sys.actorOf(scraper_dispatch.props)
