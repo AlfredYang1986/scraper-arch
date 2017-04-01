f0a68d45eb56c47e095a5a5cf5604b12390d7f95
AlfredYang1986 <yangyuanpig@gmail.com>
2017-04-01 12:06:37 +0800

    修改启动逻辑

--- src/main/scala/scraper_main.scala
+++ src/main/scala/scraper_main.scala
@@ -9,36 +9,50 @@ import dispatch.signal.dis_signal._
 import scala.io.StdIn
 
 object scarper_main extends App {
+	def actions(a : List[String]) : (Int, Option[String]) = a match {
+			case Nil => {
+
+				(-1, None)
+			}
+			case head :: tail => {
+				head match {
+					case "scraper" => (1, filePath(tail))
+					case "merge" => (0, None)
+					case _ => (-1, None)
+				}
+			}
+		}
+
+	def filePath(a : List[String]) : Option[String] = a match {
+			case Nil => None
+			case head :: _ => Some(head)
+		}
+
 	override def main(args: Array[String]): Unit = {
-		print(
-			"""scraper options:
-		  |000. scraper for dianping (大众点评)
-		  |001. scraper for weiyi (为艺)
-		  |002. scraper for victory (决胜网)
-		  |003. scraper for other (以后添加其他)
-		  |004. scraper test
-		  |005. scraper for all
-		  |006. merge existing result
-		""".stripMargin)
-
-		val str = StdIn.readLine()
-		val op = str.toInt
-
 		val path = "src/main/resources/"
-		val lst = "dianping-sketch.json" :: "weiyi-sketch.json" :: "webvictory.json" :: "other.json" :: "test.json" :: Nil
-
 		val sys = ActorSystem("scraper")
 		val s = sys.actorOf(scraper_dispatch.props)
-
-		if (op == lst.length) println("all is not implement")
-		else if (op > lst.length) {
+		val (act, f_opt) = actions(args.toList)
+		if (act < 0) {
+			println(
+				"""
+				  | you have to input option args
+				  |     scraper: crawl the web
+				  |     merge: merge existing data in data directory
+				""".stripMargin)
+			System.exit(-1)
+		} else if (act == 0) {
 			println("only merge result")
 			s ! merge_result()
-		}
-		else {
-			val f = path + lst(op)
-			println(s"json file is $f")
-			s ! start(f)
+		} else {
+			f_opt match {
+				case None => println("should have file path")
+				case Some(file) => {
+					val f = path + file
+					println(s"json file is $f")
+					s ! start(f)
+				}
+			}
 		}
 	}
 }
